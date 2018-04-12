6712d4da255f0e3076fc3df95e73cff08d1ceaac
julTorres <julientorres3@gmail.com>
2018-04-12 18:05:19 +0200

    first contact established

--- src/main/java/fr/wildcodeschool/punkapi/App.java
+++ src/main/java/fr/wildcodeschool/punkapi/App.java
@@ -1,6 +1,7 @@
 package fr.wildcodeschool.punkapi;
 
 import javax.json.Json;
+import javax.json.JsonArray;
 import javax.json.JsonReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -15,11 +16,20 @@ import java.net.URL;
  */
 public class App 
 {
-    try(InputStream is = new URL(https://api.punkapi.com/v2/).openStream();
-    JsonReader reader =  Json. createReader(new InputStreamReader(is, "UTF-8"));){
-    //TODO: Let's start doing Yoga with Json
-}
-catch(IOException e){
-    e.printStackTrace();
-}
+    public static void main(String[] args) {
+
+
+        try(InputStream is = new URL("https://api.punkapi.com/v2/beers/1").openStream();
+            JsonReader reader =  Json.createReader(new InputStreamReader(is, "UTF-8"))){
+
+            //TODO: Let's start doing Yoga with Json
+            JsonArray received = reader.readArray();
+            System.out.println(received);
+
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+
+    }
 }
