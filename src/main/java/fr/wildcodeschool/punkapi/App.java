19177eeff455f575cd9452567956342297783ae7
julTorres <julientorres3@gmail.com>
2018-04-12 16:51:24 +0200

    first commit

---
+++ src/main/java/fr/wildcodeschool/punkapi/App.java
@@ -0,0 +1,15 @@
+package fr.wildcodeschool.punkapi;
+
+/**
+ * PunkBeer
+ * Simple app for communicating with the Punk API
+ * By julienTorres
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
